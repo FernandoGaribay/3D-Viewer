3be829d4de1729912356e19cc0a3ac38f4a81178
Fernando Garibay <garibaycejaf@gmail.com>
2024-06-01 19:48:31 -0600

    + Libreria grafica actualizada

--- src/Graficos/MyGraphics.java
+++ src/Graficos/MyGraphics.java
@@ -1,14 +1,11 @@
 package Graficos;
 
 import java.awt.Color;
-import java.awt.Point;
 import java.awt.Polygon;
 import java.awt.geom.Point2D;
 import java.awt.image.BufferedImage;
 import java.util.Arrays;
 import java.util.Comparator;
-import java.util.LinkedList;
-import java.util.Queue;
 
 public class MyGraphics {
 
@@ -56,13 +53,7 @@ public class MyGraphics {
         poly.addPoint(x1, y0);
         poly.addPoint(x1, y1);
         poly.addPoint(x0, y1);
-
-        Point2D.Double puntoCentral = MyGraphicsUtils.getPuntoCentral(poly);
-        drawLine(x0, y0, x1, y0);
-        drawLine(x0, y0, x0, y1);
-        drawLine(x1, y0, x1, y1);
-        drawLine(x0, y1, x1, y1);
-        funcionRelleno((int) puntoCentral.x, (int) puntoCentral.y);
+        fillPolygon(poly);
     }
 
     public void drawPolygon(Polygon poly) {
@@ -79,62 +70,50 @@ public class MyGraphics {
     }
 
     public void fillPolygon(Polygon poly) {
-        Point2D.Double puntoCentral = MyGraphicsUtils.getPuntoCentral(poly);
-        Point2D.Double[] puntos = MyGraphicsUtils.polygonToArrayPoit2D(poly);
+        int[] xPoints = poly.xpoints;
+        int[] yPoints = poly.ypoints;
+        int nPoints = poly.npoints;
 
-        for (int i = 0; i < puntos.length; i++) {
-            int x0 = (int) puntos[i].getX();
-            int y0 = (int) puntos[i].getY();
-            int x1 = (int) puntos[(i + 1) % puntos.length].getX();
-            int y1 = (int) puntos[(i + 1) % puntos.length].getY();
-            drawLine(x0, y0, x1, y1);
+        int minY = Arrays.stream(yPoints).min().orElse(0);
+        int maxY = Arrays.stream(yPoints).max().orElse(buffer.getHeight() - 1);
+
+        for (int y = minY; y <= maxY; y++) {
+            int[] nodeX = new int[nPoints];
+            int nodes = 0;
+            int j = nPoints - 1;
+            for (int i = 0; i < nPoints; i++) {
+                if (yPoints[i] < y && yPoints[j] >= y || yPoints[j] < y && yPoints[i] >= y) {
+                    nodeX[nodes++] = (xPoints[i] + (y - yPoints[i]) * (xPoints[j] - xPoints[i]) / (yPoints[j] - yPoints[i]));
+                }
+                j = i;
+            }
+            Arrays.sort(nodeX, 0, nodes);
+            for (int i = 0; i < nodes; i += 2) {
+                if (nodeX[i] >= buffer.getWidth()) {
+                    break;
+                }
+                if (nodeX[i + 1] > 0) {
+                    if (nodeX[i] < 0) {
+                        nodeX[i] = 0;
+                    }
+                    if (nodeX[i + 1] >= buffer.getWidth()) {
+                        nodeX[i + 1] = buffer.getWidth() - 1;
+                    }
+                    for (int x = nodeX[i]; x <= nodeX[i + 1]; x++) {
+                        putPixel(x, y);
+                    }
+                }
+            }
         }
-        funcionRelleno((int) puntoCentral.x, (int) puntoCentral.y);
-
-        color = Color.red;
-        fillRect((int) (puntoCentral.x), (int) (puntoCentral.y), (int) (puntoCentral.x + 5), (int) (puntoCentral.y + 5));
     }
 
     public void fillPolygonDisordered(Polygon poly) {
-        Point2D.Double puntoCentral = MyGraphicsUtils.getPuntoCentral(poly);
-        Point2D[] puntosOrdenados = MyGraphicsUtils.ordenarPuntos(poly);
-
-        for (int i = 0; i < puntosOrdenados.length; i++) {
-            int x0 = (int) puntosOrdenados[i].getX();
-            int y0 = (int) puntosOrdenados[i].getY();
-            int x1 = (int) puntosOrdenados[(i + 1) % puntosOrdenados.length].getX();
-            int y1 = (int) puntosOrdenados[(i + 1) % puntosOrdenados.length].getY();
-            drawLine(x0, y0, x1, y1);
-        }
-        funcionRelleno((int) puntoCentral.x, (int) puntoCentral.y);
+        Point2D.Double[] puntosOrdenados = MyGraphicsUtils.ordenarPuntos(poly);
+        fillPolygon(MyGraphicsUtils.arrayPoint2DToPolygon(puntosOrdenados));
 
         color = Color.red;
+        Point2D.Double puntoCentral = MyGraphicsUtils.getPuntoCentral(poly);
         fillRect((int) (puntoCentral.x), (int) (puntoCentral.y), (int) (puntoCentral.x + 5), (int) (puntoCentral.y + 5));
-    }
-
-    public void funcionRelleno(int x, int y) {
-        putPixel(x, y);
-
-        Queue<Point> colaPixeles = new LinkedList<>();
-        colaPixeles.add(new Point(x, y));
-
-        while (!colaPixeles.isEmpty()) {
-            Point pixelActual = colaPixeles.poll();
-            int tempX = pixelActual.x;
-            int tempY = pixelActual.y;
-
-            aniadirPixel(tempX, tempY + 1, colaPixeles); // Arriba
-            aniadirPixel(tempX + 1, tempY, colaPixeles); // Derecha
-            aniadirPixel(tempX, tempY - 1, colaPixeles); // Abajo
-            aniadirPixel(tempX - 1, tempY, colaPixeles); // Izquierda
-        }
-    }
-
-    private void aniadirPixel(int x, int y, Queue<Point> queue) {
-        if (x >= 0 && x < buffer.getWidth() && y >= 0 && y < buffer.getHeight() && buffer.getRGB(x, y) != color.getRGB()) {
-            putPixel(x, y);
-            queue.add(new Point(x, y));
-        }
     }
 
     private void putPixel(int x, int y) {
@@ -159,8 +138,8 @@ public class MyGraphics {
         return this.buffer;
     }
 
-    public void getColorAt(int x, int y) {
-        System.out.println(buffer.getRGB(x, y));
+    public int getColorAt(int x, int y) {
+        return buffer.getRGB(x, y);
     }
 }
 
@@ -185,15 +164,26 @@ class MyGraphicsUtils {
         return puntoCentral;
     }
 
-    public static Point2D.Double[] polygonToArrayPoit2D(Polygon poly) {
+    public static Point2D.Double[] polygonToArrayPoint2D(Polygon poly) {
         int nPoints = poly.npoints;
-        Point2D.Double[] points = new Point2D.Double[nPoints];
 
+        Point2D.Double[] points = new Point2D.Double[nPoints];
         for (int i = 0; i < nPoints; i++) {
             points[i] = new Point2D.Double(poly.xpoints[i], poly.ypoints[i]);
         }
 
         return points;
+    }
+
+    public static Polygon arrayPoint2DToPolygon(Point2D.Double[] points) {
+        int nPoints = points.length;
+
+        Polygon tempPoly = new Polygon();
+        for (int i = 0; i < nPoints; i++) {
+            tempPoly.addPoint((int) points[i].getX(), (int) points[i].getY());
+        }
+
+        return tempPoly;
     }
 
     // algoritmo de Graham Scan
