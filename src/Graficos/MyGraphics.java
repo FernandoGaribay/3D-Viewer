26384ab7e3a20880fc84e9385efc9f27f7122462
Fernando Garibay <garibaycejaf@gmail.com>
2024-06-02 20:05:51 -0600

    + Caras ocultas con ZBuffer

--- src/Graficos/MyGraphics.java
+++ src/Graficos/MyGraphics.java
@@ -6,9 +6,11 @@ import java.awt.geom.Point2D;
 import java.awt.image.BufferedImage;
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.HashMap;
 
 public class MyGraphics {
 
+    private HashMap<Point2D.Double, Double> zBuffer;
     private BufferedImage buffer;
     private Color color;
     private int WIDTH;
@@ -16,6 +18,7 @@ public class MyGraphics {
 
     public MyGraphics(int width, int height) {
         this.buffer = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+        this.zBuffer = new HashMap<>();
         this.color = new Color(0, 0, 0);
         this.WIDTH = width;
         this.HEIGHT = height;
@@ -37,6 +40,50 @@ public class MyGraphics {
             putPixel(Math.round(x), Math.round(y));
             x += xIncremento;
             y += yIncremento;
+        }
+    }
+
+    public void drawLine3D(Point2D.Double p1, double z1, Point2D.Double p2, double z2, Point2D.Double puntoFuga) {
+        Point2D.Double p1Proj = p1;
+        Point2D.Double p2Proj = p2;
+
+        int x0 = (int) p1Proj.getX();
+        int y0 = (int) p1Proj.getY();
+        double z0 = z1;
+        int x1 = (int) p2Proj.getX();
+        int y1 = (int) p2Proj.getY();
+        double z1Proj = z2;
+
+        int dx = Math.abs(x1 - x0);
+        int dy = Math.abs(y1 - y0);
+        int sx = x0 < x1 ? 1 : -1;
+        int sy = y0 < y1 ? 1 : -1;
+        int err = dx - dy;
+
+        double dz = dx > dy ? Math.abs(z1Proj - z0) / dx : Math.abs(z1Proj - z0) / dy;
+        if (z1Proj < z0) {
+            dz = -dz;
+        }
+
+        while (true) {
+            if (x0 >= 0 && x0 < WIDTH && y0 >= 0 && y0 < HEIGHT) {
+                putPixel(x0, y0, z0);
+            }
+
+            if (x0 == x1 && y0 == y1) {
+                break;
+            }
+            int e2 = 2 * err;
+            if (e2 > -dy) {
+                err -= dy;
+                x0 += sx;
+                z0 += dz;  // Update z along x
+            }
+            if (e2 < dx) {
+                err += dx;
+                y0 += sy;
+                z0 += dz;  // Update z along y
+            }
         }
     }
 
@@ -88,7 +135,7 @@ public class MyGraphics {
                 j = i;
             }
             Arrays.sort(nodeX, 0, nodes);
-            for (int i = 0; i < nodes; i += 2) {
+            for (int i = 0; i < 1; i += 2) {
                 if (nodeX[i] >= buffer.getWidth()) {
                     break;
                 }
@@ -101,6 +148,45 @@ public class MyGraphics {
                     }
                     for (int x = nodeX[i]; x <= nodeX[i + 1]; x++) {
                         putPixel(x, y);
+                    }
+                }
+            }
+        }
+    }
+
+    public void fillPolygon(Polygon poly, double midZIndex) {
+        int[] xPoints = poly.xpoints;
+        int[] yPoints = poly.ypoints;
+        int nPoints = poly.npoints;
+
+        int minY = Arrays.stream(yPoints).min().orElse(0);
+        int maxY = Arrays.stream(yPoints).max().orElse(buffer.getHeight() - 1);
+
+        for (int y = minY; y <= maxY; y++) {
+            int[] nodeX = new int[nPoints];
+            int nodes = 0;
+            int j = nPoints - 1;
+            for (int i = 0; i < nPoints; i++) {
+                if (yPoints[i] < y && yPoints[j] >= y || yPoints[j] < y && yPoints[i] >= y) {
+                    nodeX[nodes++] = (xPoints[i] + (y - yPoints[i]) * (xPoints[j] - xPoints[i]) / (yPoints[j] - yPoints[i]));
+                }
+                j = i;
+            }
+            Arrays.sort(nodeX, 0, nodes);
+            for (int i = 0; i < nodes; i += 2) {
+                if (nodeX[i] >= buffer.getWidth()) {
+                    break;
+                }
+                if (nodeX[i + 1] > 0) {
+                    if (nodeX[i] < 0) {
+                        nodeX[i] = 0;
+                    }
+                    if (nodeX[i + 1] >= buffer.getWidth()) {
+                        nodeX[i + 1] = buffer.getWidth() - 1;
+                    }
+                    for (int x = nodeX[i]; x <= nodeX[i + 1]; x++) {
+                        Point2D.Double zBufferPoint = new Point2D.Double(x, y);
+                        putPixel(x, y, midZIndex);
                     }
                 }
             }
@@ -122,6 +208,16 @@ public class MyGraphics {
         }
     }
 
+    private void putPixel(int x, int y, double z) {
+        if (x >= 0 && x < buffer.getWidth() && y >= 0 && y < buffer.getHeight()) {
+            Point2D.Double point = new Point2D.Double(x, y);
+            if (!zBuffer.containsKey(point) || zBuffer.get(point) > z) {
+                zBuffer.put(point, z);
+                buffer.setRGB(x, y, color.getRGB());
+            }
+        }
+    }
+
     public Color getColor() {
         return color;
     }
@@ -132,10 +228,19 @@ public class MyGraphics {
 
     public void resetBuffer() {
         buffer = new BufferedImage(HEIGHT, WIDTH, BufferedImage.TYPE_INT_ARGB);
+        zBuffer = new HashMap<>();
     }
 
     public BufferedImage getBuffer() {
         return this.buffer;
+    }
+
+    public HashMap<Point2D.Double, Double> getzBuffer() {
+        return zBuffer;
+    }
+
+    public void setzBuffer(HashMap<Point2D.Double, Double> zBuffer) {
+        this.zBuffer = zBuffer;
     }
 
     public int getColorAt(int x, int y) {
